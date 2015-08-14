51765342d295211ebb6893d9de364dc3812d9143
tmont <tommy.mont@gmail.com>
2015-08-14 15:32:40 -0700

    factored base out of modulus function

--- src/modulus.js
+++ src/modulus.js
@@ -5,14 +5,15 @@ module.exports = function(value, mod) {
 		return 0;
 	}
 
-	var exp = Math.log(Math.min(Math.abs(value), mod)) / Math.log(10);
+	var base = 10;
+	var exp = Math.log(Math.min(Math.abs(value), mod)) / Math.log(base);
 	var multiplier = 1;
 	if (exp < 0) {
-		multiplier = Math.pow(10, Math.abs(Math.round(exp)));
+		multiplier = Math.pow(base, Math.abs(Math.round(exp)));
 		value = multiplier * value;
 		mod = multiplier * mod;
 	}
 
 	var result = ((mod + (value % mod)) % mod) / multiplier;
-	return roundProperly(result, Math.abs(Math.floor(exp)) + 10, 10);
+	return roundProperly(result, Math.abs(Math.floor(exp)) + 1, base);
 };
